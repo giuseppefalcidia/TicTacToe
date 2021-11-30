bf3f2795dffc8dda20bc1b8d46969b3e27040122
Edith-Heidmann <eheidmann@gmail.com>
2021-11-30 14:30:28 +0100

    Added Component and styling structure - to merge to main

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import React from "react";
+import LandingPage from "./Components/LandingPage";
+import GamePage from "./Components/GamePage";
+
 import "./Styling/App.scss";
 
 function App() {
   return (
     <div className="app-container">
       <header className="app-header">
-        <h1 className="main-heading">Tic Tac Toe</h1>
-
-        <div></div>
+        <h1 className="app-heading">Tic Tac Toe</h1>
       </header>
+      <main className="main-container">
+        <LandingPage />
+
+        {/* // !!! Conditional rendering to come.. so GamePage will only open once loged in - can still be styled and elements added :)  */}
+
+        <GamePage />
+      </main>
     </div>
   );
 }
---
+++ src/Components/GamePage.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const GamePage = () => {
+  return (
+    <div className="game-page-container">
+      <div className="game-container">
+        <div className="grid-component">1</div>
+        <div className="grid-component">2</div>
+        <div className="grid-component">3</div>
+        <div className="grid-component">3</div>
+        <div className="grid-component">5</div>
+        <div className="grid-component">6</div>
+        <div className="grid-component">7</div>
+        <div className="grid-component">8</div>
+        <div className="grid-component">9</div>
+      </div>
+    </div>
+  );
+};
+
+export default GamePage;
