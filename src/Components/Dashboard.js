12fb4af6d613b9955a356b1b3ecee3be8f0b0af9
Janowitz <janhapple@outlook.de>
2021-12-01 15:36:00 +0100

    updated dashboard

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -10,9 +10,23 @@ const Dashboard = (props) => {
     return(
         <div>
             <h1>Welcome {props.username}!</h1>
-            <p>Let's play Tic-Tac-Toe</p>
 
             <button onClick={handleEditUser}>edit Userdate</button>
+            {editUser === false ? 
+            (<p>Let's play Tic-Tac-Toe</p>) 
+            : 
+            (
+            <div>
+                <p>Let's play Tic-Tac-Toe</p>
+                <form>
+                    <div>
+                        <label id="change-password"></label>
+                        <input type="text" id="change-password"></input>
+                        <button>confirm</button>
+                    </div>
+                </form>
+            </div>
+            )}
 
             <button>Start Game</button>
         </div>
