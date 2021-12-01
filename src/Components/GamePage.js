4188b276091ce9b7bc02a99d8fc7c3162ab8d85e
giuseppefalcidia <68688135+giuseppefalcidia@users.noreply.github.com>
2021-12-01 11:54:06 +0100

    Update GamePage.js

--- src/Components/GamePage.js
+++ src/Components/GamePage.js
@@ -1,20 +1,57 @@
 import React from "react";
+import '../Styling/game-page.scss';
+import  '../GamePageFunctionality/GamePageMainFunc.js';
 
 const GamePage = () => {
   return (
-    <div className="game-page-container">
-      <div className="game-container">
-        <div className="grid-component">1</div>
-        <div className="grid-component">2</div>
-        <div className="grid-component">3</div>
-        <div className="grid-component">3</div>
-        <div className="grid-component">5</div>
-        <div className="grid-component">6</div>
-        <div className="grid-component">7</div>
-        <div className="grid-component">8</div>
-        <div className="grid-component">9</div>
+    <>
+  <meta charSet="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Tic Tac Toe</title>
+  <link rel="stylesheet" href="css/style.css" />
+  <div id="container">
+    {/* Starting Page */}
+    <div id="startingPage">
+      <h2>Tic Tac Toe</h2>
+      <div id="line" />
+      <h4>Select Which You Want To Be?</h4>
+      <div id="button">
+        <button id="playerX" className="choose">
+          Player ( X )
+        </button>
+        <button id="playerO" className="choose">
+          Player ( O )
+        </button>
       </div>
     </div>
+    {/* Main Page */}
+    <div id="mainPage">
+      <div id="headerBtns">
+        <button id>X Turn</button>
+        <button id="O_Turn">O Turn</button>
+        <div id="showChange" />
+      </div>
+      <div id="gameBoard">
+        <div className="boxes" />
+        <div className="boxes" />
+        <div className="boxes" />
+        <div className="boxes" />
+        <div className="boxes" />
+        <div className="boxes" />
+        <div className="boxes" />
+        <div className="boxes" />
+        <div className="boxes" />
+      </div>
+    </div>
+    {/* WInner Page */}
+    <div id="winner">
+      <h2 id="winnerName">Player X Win The Game!</h2>
+      <div id="button">
+        <button id="quit">Play Again</button>
+      </div>
+    </div>
+  </div>
+</>
   );
 };
 
