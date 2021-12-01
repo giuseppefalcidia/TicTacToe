c5a2d67633a3b6f72c4446706d57598ebac80f86
Marc Lopez <marclopeztorrent@gmail.com>
2021-12-01 16:57:20 +0100

    solved inline styling problems

--- src/Components/GamePage.js
+++ src/Components/GamePage.js
@@ -1,5 +1,7 @@
 import React, {useState } from "react";
-// import "../Styling/game-page.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleNotch, faTimes  } from '@fortawesome/free-solid-svg-icons'
+import "../Styling/game-page.scss";
 
 const GamePage = () => {
   const [winnerName,setWinnerName] = useState("")
@@ -8,13 +10,18 @@ const GamePage = () => {
   const [startingPage,setStartingPage] = useState(true)
   const [mainPage,setMainPage] = useState(true)
   const [winnerPage, setWinnerPage] = useState(false)
+  const [isCircle,setIsCircle] = useState(null) //! null after test
+
+  const circle = <FontAwesomeIcon icon={faCircleNotch} />
+  const ex = <FontAwesomeIcon icon={faTimes} />
+  // TODO
+  let boxes = document.querySelectorAll(".boxes");
 
 
-const handleChoose = () => {
-  if (e.currentTarget.id === "playerX") {
+const handleChoose = (event) => {
+  if (event.currentTarget.id === "playerX") {
     setChangeTurn(false);
-    setShowChange()
-    showChange(true);
+    setShowChange(true);
   } else {
     setChangeTurn(true);
     setShowChange(false);
@@ -24,19 +31,23 @@ const handleChoose = () => {
 }
 
 const handleBoxClick = (event) => {
+  console.log(event)
+  const element = <FontAwesomeIcon icon={faCircleNotch} />
   if (changeTurn === false) {
-    e.currentTarget.innerHTML = `<i class="fas fa-times"></i>`;
-    e.currentTarget.id = "X";
-    e.currentTarget.style.pointerEvents = "none";
+    console.log(ex)
+    // event.currentTarget.innerHTML = ;
+    event.currentTarget.id = "X";
+    event.currentTarget.style.pointerEvents = "none";
     setShowChange(true) 
     setChangeTurn(true)
+    setIsCircle(false)
   } else {
-    e.currentTarget.innerHTML = `<i class="fas fa-circle-notch"></i>`;
-    e.currentTarget.id = "O";
-    e.currentTarget.pointerEvents = "none";
+    // event.currentTarget.innerHTML = circle;
+    event.currentTarget.id = "O";
+    event.currentTarget.pointerEvents = "none";
     setShowChange(false)
-
-    setChangeTurn(false);
+    setChangeTurn(false)
+    setIsCircle(true)
   }
   winningFunc();
   drawFunc();
@@ -70,7 +81,7 @@ let winningFunc = () => {
       boxes[b[2]].id === "X"
     ) {
 
-      serWinnerName(`Player X Win The Game!`);
+      setWinnerName(`Player X Win The Game!`);
 
 
       setMainPage(false)
@@ -126,41 +137,41 @@ const handleQuit = () => {
       <link rel="stylesheet" href="css/style.css" />
       <div id="container">
         {/* Starting Page */}
-        <div id="startingPage" style={ startingPage ? display="block" : display="none" }>
-          <h2>Tic Tac Toe</h2>
+        <div id="startingPage" style={ startingPage ? {display: "block"} : {display: "none"} }> 
+        <h2>Tic Tac Toe</h2>
           <div id="line" />
           <h4>Select Which You Want To Be?</h4>
           <div id="button">
-            <button id="playerX" className="choose" onClick={handleChoose()}>
+            <button id="playerX" className="choose" onClick={event => handleChoose(event)}>
               Player ( X )
             </button>
-            <button id="playerO" className="choose" onClick={handleChoose()}>
+            <button id="playerO" className="choose" onClick={event => handleChoose(event)}>
               Player ( O )
             </button>
           </div>
         </div>
         {/* Main Page */}
-        <div id="mainPage" style={ mainPage ? display="block" : display="none"}>
+        <div id="mainPage" style={ mainPage ? {display: "block"} : {display: "none"}}>
           <div id="headerBtns">
             <button id>X Turn</button>
             <button id="O_Turn">O Turn</button>
-            <div id="showChange" style={ showChange ? left = `0px` : `left = 160px`  }/>
+            <div id="showChange" style={ showChange ? {left: "0px"} : {left: "160px"}  }/>
           </div>
           <div id="gameBoard">
-            <div className="boxes" onClick={handleBoxClick}/>
-            <div className="boxes" onClick={handleBoxClick}/>
-            <div className="boxes" onClick={handleBoxClick}/>
-            <div className="boxes" onClick={handleBoxClick}/>
-            <div className="boxes" onClick={handleBoxClick}/>
-            <div className="boxes" onClick={handleBoxClick}/>
-            <div className="boxes" onClick={handleBoxClick}/>
-            <div className="boxes" onClick={handleBoxClick}/>
-            <div className="boxes" onClick={handleBoxClick}/>
+            <div className="boxes" onClick={event => handleBoxClick(event)}></div>
+            <div className="boxes" onClick={event => handleBoxClick(event)}></div>
+            <div className="boxes" onClick={event => handleBoxClick(event)}></div>
+            <div className="boxes" onClick={event => handleBoxClick(event)}></div>
+            <div className="boxes" onClick={event => handleBoxClick(event)}></div>
+            <div className="boxes" onClick={event => handleBoxClick(event)}></div>
+            <div className="boxes" onClick={event => handleBoxClick(event)}></div>
+            <div className="boxes" onClick={event => handleBoxClick(event)}></div>
+            <div className="boxes" onClick={event => handleBoxClick(event)}></div>
           </div>
         </div>
-        {/* WInner Page */}
-        <div id="winner">
-          <h2 id="winnerName">Player X Win The Game!</h2>
+        {/* Winner Page */}
+        <div id="winner" style={ winnerPage ? {display: "block"} : {display: "none"}}>
+          <h2 id="winnerName">{winnerName}</h2>
           <div id="button">
             <button onClick={handleQuit()} id="quit">Play Again</button>
           </div>
