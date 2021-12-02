5926ae019066ca90aeae61902afe23691a48ea2a
JanHapple <janhapple@outlook.de>
2021-12-02 11:30:11 +0100

    added new function with useNavigation to Landingpage

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -2,212 +2,15 @@ import React from "react";
 import { useNavigate, Link } from "react-router-dom";
 
 const LandingPage = (props) => {
-  let navigate = useNavigate();
-
-  const redirectToDashboard = () => {
-    if (props.currentUser.username.length > 0) {
+  
+  const RedirectToDashboard = async (event) => {
+    let navigate = useNavigate();
+    event.preventDefault();
+    if (props.currentUser.username) {
+      await props.submitLoginData(event.target)
       navigate("/dashboard", {replace: true})
     }
   }
-  // // State hooks for login and sign up
-  // const [username, setUsername] = useState("");
-  // const [password, setPassword] = useState("");
-
-  // const [signUpArea, setSignUpAre] = useState(false);
-
-  // // !!! Write code for 'if sign up then display none on login and display  Sign up form
-  // const signUpDisplay = React.createRef();
-
-  // // todo - Current User Object
-  // const [currentUser, setCurrentUser] = useState({
-  //   _id: "",
-  //   username: "",
-  //   // todo - Placeholders for future state to store..
-  //   // score: "",
-  //   // rank: "",
-  // });
-
-  // // Function to change the state variable corresponding to a form input the user tried to change
-  // const updateData = (event) => {
-  //   // Switch
-  //   switch (event.target.name) {
-  //     case "username":
-  //       setUsername(event.target.value);
-  //       break;
-  //     case "password":
-  //       setPassword(event.target.value);
-  //       break;
-  //     case "email":
-  //       setEmail(event.target.value);
-  //       break;
-  //     case "signUp":
-  //       signUpArea(event.target.value);
-  //       break;
-
-  //     default:
-  //       break;
-  //   }
-  // };
-
-  // // ! LOGIN functionality
-  // // todo - POST and FETCH request for login - connect to database and setup correct path
-  // // Submit login data function - updated onChange..
-  // const submitLoginData = (event) => {
-  //   event.preventDefault();
-
-  //   // ! Should I split into different variables for login and sign up
-  //   // Collected Data from update data...
-  //   const loginData = {
-  //     username: username,
-  //     password: password,
-  //   };
-
-  //   //  For testing...
-  //   console.log(loginData.username);
-  //   console.log(loginData.password);
-
-  //   // todo - uncomment once set up with db
-  //   const jsonLoginData = JSON.stringify(loginData);
-
-  //   const settings = {
-  //     method: "POST",
-  //     body: jsonLoginData,
-  //     credentials: "include",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //   };
-
-  //   // todo - set path
-  //   fetch("http://localhost:5000/login", settings)
-  //     .then((response) => {
-  //       if (response.ok) {
-  //         console.log(response);
-  //         return response.json();
-  //       } else {
-  //         switch (response.status) {
-  //           case 401:
-  //             return response.json().then((err) => {
-  //               throw new Error(err.message);
-  //             });
-  //           default:
-  //             throw new Error("unknown");
-  //         }
-  //       }
-  //     })
-  //     .then((data) => {
-  //       setCurrentUser(data);
-  //       setUsername("");
-  //       setPassword("");
-  //       // if (data.token) {
-  //       //  navigate('/dashboard')
-  //       // }
-  //     })
-  //     .catch((err) => {
-  //       alert(err.message);
-  //       setUsername("");
-  //       setPassword("");
-  //     });
-  // };
-
-  // // !! ======
-
-  // const addLoginData = (event) => {
-  //   event.preventDefault();
-
-  //   // Collected Data from update data...
-  //   const loginData = {
-  //     username: username,
-  //     password: password,
-  //     email: email,
-  //   };
-
-  //   //  For testing...
-  //   // console.log(loginData.username);
-  //   // console.log(loginData.password);
-
-  //   // todo - uncomment once set up with db
-  //   const jsonLoginData = JSON.stringify(loginData);
-
-  //   const settings = {
-  //     method: "POST",
-  //     body: jsonLoginData,
-  //     credentials: "include",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //   };
-
-  //   // todo - set path
-  //   fetch("http://localhost:5000/user", settings)
-  //     .then((response) => {
-  //       if (response.ok) {
-  //         console.log(response);
-  //         return response.json();
-  //       } else {
-  //         switch (response.status) {
-  //           case 401:
-  //             return response.json().then((err) => {
-  //               throw new Error(err.message);
-  //             });
-  //           default:
-  //             throw new Error("unknown");
-  //         }
-  //       }
-  //     })
-  //     .then((data) => {
-  //       // !! Check back with currentUser
-  //       console.log(data);
-  //       // setCurrentUser(data);
-  //       setUsername("");
-  //       setPassword("");
-  //       setEmail("");
-  //       // if (data.token) {
-  //       //  navigate('/dashboard')
-  //       // }
-  //     })
-  //     .catch((err) => {
-  //       alert(err.message);
-  //       setUsername("");
-  //       setPassword("");
-  //       setEmail("");
-  //     });
-  // };
-
-  // !!! Write code for 'if sign up then display none on login and display  Sign up form
-  //   const displaySignIn = (event) => {
-  //     event.preventDefault();
-
-  // if(signUpArea){
-
-  //     return (
-  //       <>
-  //         <div className="sign-in-div" ref={signUpDisplay}>
-  //           <form className="sign-up-form" onSubmit={submitLoginData}>
-  //             <label>Username</label>
-  //             <input
-  //               name="username"
-  //               onChange={updateData}
-  //               value={username}
-  //             ></input>
-
-  //             <label>Email</label>
-  //             <input name="email" onChange={updateData} value={email}></input>
-
-  //             <label>Password</label>
-  //             <input
-  //               name="password"
-  //               onChange={updateData}
-  //               value={password}
-  //             ></input>
-
-  //             <button>Sign me up!</button>
-  //           </form>
-  //         </div>
-  //       </>
-  //     );
-  //     }
-  //   };
 
   return (
     <div className="landing-page-container">
@@ -239,7 +42,7 @@ const LandingPage = (props) => {
               value={props.password}
             ></input>
 
-            <button onClick={redirectToDashboard}>Login</button>
+            <button onClick={RedirectToDashboard}>Login</button>
           </form>
         </div>
 
